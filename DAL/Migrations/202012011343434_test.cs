adc6f36b04edf2a3c2ceddb4220650893b6c69c2
Paweł Łęczycki <pawel@msd.info.pl>
2020-12-01 14:51:29 +0100

    DAL (Database First)

---
+++ DAL/Migrations/202012011343434_test.cs
@@ -0,0 +1,18 @@
+﻿namespace DAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class test : DbMigration
+    {
+        public override void Up()
+        {
+            DropColumn("dbo.Students", "IndexNumber");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Students", "IndexNumber", c => c.Int(nullable: false));
+        }
+    }
+}
