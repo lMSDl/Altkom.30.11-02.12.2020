e782902a6f2e461536196e9b7daf11440667ae51
Paweł Łęczycki <pawel@msd.info.pl>
2020-12-02 15:11:27 +0100

    Indexer, Nullable

---
+++ ConsoleApp/Indexers/Indexer.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Indexers
+{
+    public class Indexer
+    {
+        public class StringDataStore
+        {
+            private string[] store = new string[10];
+
+            public string this[int index]
+            {
+                get
+                {
+                    if (index >= store.Length || index < 0)
+                        return null;
+                    return store[index];
+                }
+                set
+                {
+                    if (index < store.Length && index >= 0)
+                         store[index] = value;
+                }
+            }
+
+            public int this[string data]
+            {
+                get
+                {
+                    return store.ToList().IndexOf(data);
+                }
+            }
+
+
+            public void SetString(int index, string value)
+            {
+                store[index] = value;
+            }
+
+            public string GetString(int index, string value)
+            {
+                return store[index];
+            }
+        }
+
+        public void Test()
+        {
+            var dataStore = new[] { new StringDataStore(), new StringDataStore() };
+            dataStore[0].SetString(0, "1");
+            dataStore[0].SetString(1, "two");
+            dataStore[0].SetString(2, "III");
+
+            dataStore[0][3] = "4";
+            dataStore[0][4] = "five";
+            dataStore[0][5] = "VI";
+
+            for(int i = 0; i < 10; i++)
+                Console.WriteLine(dataStore[0][i]);
+
+            var index = dataStore[0]["five"];
+            Console.WriteLine(index);
+           
+        }
+
+    }
+}
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using ConsoleApp.Lambda;
+﻿using ConsoleApp.Indexers;
+using ConsoleApp.Lambda;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -27,10 +28,35 @@ namespace ConsoleApp
             new Linq().Test();
 
 
-            var service = new CalculatorService.CalculatorClient();
-            Console.WriteLine($"2*5={service.Multiply(2, 5)}");
+            //var service = new CalculatorService.CalculatorClient();
+            //Console.WriteLine($"2*5={service.Multiply(2, 5)}");
 
-            Console.WriteLine(string.Join( " ", new EducatorsService.EducatorsServiceClient().Read().Select(x => x.FirstName + x.LastName)));
+            //Console.WriteLine(string.Join( " ", new EducatorsService.EducatorsServiceClient().Read().Select(x => x.FirstName + x.LastName)));
+
+            new Indexer().Test();
+
+
+            Nullable<int> a = null;
+            int? b = 5;
+            int c;
+
+            if(a - b == 0 || a - b == null)
+            {
+                c = (a + b) ?? 0;
+            }
+            else
+            {
+                var result = a - b;
+                // if (result != null)
+                if (result.HasValue)
+                    c = result.Value;
+                else
+                    c = 0;
+            }
+
+            c = (a - b == 0 || a - b == null) ? (a - b) ?? 0 : (a + b) ?? 0;
+            c = ((a - b == 0 || a - b == null) ? (a - b) : (a + b)) ?? 0;
+
         }
     }
 }
