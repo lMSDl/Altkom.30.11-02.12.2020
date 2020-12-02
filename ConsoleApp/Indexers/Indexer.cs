aef3d4d258b0281ee11a8a7d72cb554547e21001
Paweł Łęczycki <pawel@msd.info.pl>
2020-12-02 15:30:24 +0100

    operator konwersji

--- ConsoleApp/Indexers/Indexer.cs
+++ ConsoleApp/Indexers/Indexer.cs
@@ -10,7 +10,17 @@ namespace ConsoleApp.Indexers
     {
         public class StringDataStore
         {
-            private string[] store = new string[10];
+            public StringDataStore() : this(new string[10])
+            {
+
+            }
+
+            public StringDataStore(string[] array)
+            {
+                store = array;
+            }
+
+            private string[] store;
 
             public string this[int index]
             {
@@ -23,7 +33,7 @@ namespace ConsoleApp.Indexers
                 set
                 {
                     if (index < store.Length && index >= 0)
-                         store[index] = value;
+                        store[index] = value;
                 }
             }
 
@@ -45,11 +55,15 @@ namespace ConsoleApp.Indexers
             {
                 return store[index];
             }
+
+            public static implicit operator string[] (StringDataStore x) => x.store;
+            public static explicit operator StringDataStore(string[] x) => new StringDataStore(x);
         }
 
         public void Test()
         {
-            var dataStore = new[] { new StringDataStore(), new StringDataStore() };
+
+            var dataStore = new[] { new StringDataStore(), (StringDataStore)new string[5] };
             dataStore[0].SetString(0, "1");
             dataStore[0].SetString(1, "two");
             dataStore[0].SetString(2, "III");
@@ -63,7 +77,11 @@ namespace ConsoleApp.Indexers
 
             var index = dataStore[0]["five"];
             Console.WriteLine(index);
-           
+
+            string[] result = dataStore[0];
+
+
+
         }
 
     }
