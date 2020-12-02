6a000bfa375eddd0b162f115304554398b42a784
Paweł Łęczycki <pawel@msd.info.pl>
2020-12-02 14:07:46 +0100

    COM

---
+++ WpfApp/WordDoc.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Word;
+
+namespace WpfApp
+{
+    public class WordDoc : IDisposable
+    {
+        Application _word = null;
+
+        public WordDoc()
+        {
+            _word = new Application() { Visible = false };
+        }
+
+        public void CreateDocument()
+        {
+            var doc = _word.Documents.Add();
+            doc.Activate();
+        }
+
+        public void AppendText(string text)
+        {
+            var location = _word.ActiveDocument.Range(_word.ActiveDocument.Content.End - 1);
+
+            location.Bold = 1;
+            location.Italic = 1;
+            location.InsertAfter(text);
+        }
+
+        public void SaveAs(string location)
+        {
+            var document = _word.ActiveDocument;
+            document.SaveAs(location);
+            document.Close();
+        }
+
+        public void Dispose()
+        {
+            if (_word != null)
+            {
+                _word.Quit();
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(_word);
+                _word = null;
+                System.GC.SuppressFinalize(this);
+            }
+        }
+    }
+}
